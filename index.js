dc66ec75b71d5831fff6b8e67f2869937f0adb27
ghanemxghanem@gmail.com <ghanemxghanem@gmail.com>
2021-11-30 21:38:45 -0500

    connected bot to the server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const Discord = require("discord.js")
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.on("ready",()=> {
+  console.log('logged in as $ {client.user.tag}!')
+})
+
+
+client.on("message", msg => {
+  if (msg.conent == "ping"){
+    msg,reply("hello, you need something?")
+  }
+})
+
+//console.log(process.env.TOKEN)
+client.login(process.env.TOKEN)
